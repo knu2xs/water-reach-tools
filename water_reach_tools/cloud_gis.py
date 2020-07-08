76aece9dac18fea6682c376ee48c4ddef2a57231
Joel McCune <knu2xs@gmail.com>
2020-07-08 12:16:29 -0700

    Rearrange object to be cleaner

--- water_reach_tools/cloud_gis.py
+++ water_reach_tools/cloud_gis.py
@@ -7,10 +7,11 @@ from dotenv import load_dotenv, find_dotenv
 # get the dotenv files from any parent directories
 load_dotenv(find_dotenv())
 
+
 class ReachGIS(GIS):
 
     def __init__(self, url=None, username=None, password=None, key_file=None, cert_file=None, verify_cert=True, set_active=True, client_id=None, profile=None, **kwargs):
-        
+
         # if not explicitly specified, try to get the necessary information from a dotenv
         if not url:
             url = os.getenv('GIS_URL')
@@ -18,17 +19,17 @@ class ReachGIS(GIS):
         if not username and not password:
             username = os.getenv('GIS_USERNAME')
             password = os.getenv('GIS_PASSWORD')
-        
+
         super().__init__(url=url, username=username, password=password, key_file=key_file, cert_file=cert_file, verify_cert=verify_cert, set_active=set_active, client_id=client_id, profile=profile, **kwargs)
 
         # attempt to load the respective relevant feature layers from the dotenv file
-        centroid_id = os.getenv('CENTROID_FEATURE_LAYER_ID')        
+        centroid_id = os.getenv('REACH_CENTROID_ID')
         self.centroid_feature_layer = self._get_feature_layer_by_object_id(centroid_id) if centroid_id else None
 
-        line_id = os.getenv('LINE_FEATURE_LAYER_ID')
+        line_id = os.getenv('REACH_LINE_ID')
         self.line_feature_layer = self._get_feature_layer_by_object_id(line_id) if line_id else None
 
-        points_id = os.getenv('POINTS_FEATURE_LAYER_ID')
+        points_id = os.getenv('REACH_POINTS_ID')
         self.points_feature_layer = self._get_feature_layer_by_object_id(points_id) if points_id else None
 
     def _get_feature_layer_by_object_id(self, object_id):
--- water_reach_tools/water_reach_tools.py
+++ water_reach_tools/water_reach_tools.py
@@ -5,29 +5,19 @@ class Reach(object):
 
     def __init__(self, reach_id):
 
-        self.reach_id = str(reach_id)
-        self.reach_name = ''
-        self.reach_name_alternate = ''
-        self.river_name = ''
-        self.river_name_alternate = ''
-        self.error = None                   # boolean
-        self.notes = ''
+        self._geometry = None
+        self._reach_points = []
+
+        self.abstract = ''
+        self.agency = None
+        self.description = ''
         self.difficulty = ''
         self.difficulty_minimum = ''
         self.difficulty_maximum = ''
         self.difficulty_outlier = ''
-        self.abstract = ''
-        self.description = ''
-        self.update_aw = None               # datetime
-        self.update_arcgis = None           # datetime
-        self.validated = None               # boolean
-        self.validated_by = ''
-        self._geometry = None
-        self._reach_points = []
-        self.agency = None
+        self.error = None                   # boolean
         self.gauge_observation = None
         self.gauge_id = None
-        self.gauge_units = None
         self.gauge_metric = None
         self.gauge_r0 = None
         self.gauge_r1 = None
@@ -39,5 +29,16 @@ class Reach(object):
         self.gauge_r7 = None
         self.gauge_r8 = None
         self.gauge_r9 = None
+        self.gauge_units = None
+        self.notes = ''
+        self.reach_id = str(reach_id)
+        self.reach_name = ''
+        self.reach_name_alternate = ''
+        self.river_name = ''
+        self.river_name_alternate = ''
         self.tracing_method = None
         self.trace_source = None
+        self.update_aw = None               # datetime
+        self.update_arcgis = None           # datetime
+        self.validated = None               # boolean
+        self.validated_by = ''
