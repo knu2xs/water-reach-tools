438bdd181b49f93a1a4cf7397452af2be252efc5
Joel McCune <knu2xs@gmail.com>
2020-07-08 10:29:54 -0700

    Scaffold out requirements and tests

--- tests/test_water_reach_tools.py
+++ tests/test_water_reach_tools.py
@@ -1,11 +1,41 @@
 #!/usr/bin/env python
 
 """Tests for `water_reach_tools` package."""
+import os
 
+from arcgis.gis import GIS
+from dotenv import load_dotenv
 import pytest
 
-
 from water_reach_tools import water_reach_tools
+
+# load the environment variables using dotenv
+load_dotenv('../.env')
+
+@pytest.fixture
+def gis():
+    """Instantiate the GIS object"""
+    gis = GIS(
+        url=os.getenv('ARCGIS_URL', 'https://arcgis.com'),
+        username=os.getenv('ARCGIS_USERNAME', None),
+        password=os.getenv('ARCGIS_PASSWORD', None)
+    )
+    return gis
+
+
+@pytest.fixture
+def reach_centroid_lyr(gis):
+    return gis.content.get(os.getenv('REACH_CENTROID_ID'))
+
+
+@pytest.fixture
+def reach_line_lyr(gis):
+    return gis.content.get(os.getenv('REACH_LINE_ID'))
+
+
+@pytest.fixture
+def reach_points_lyr(gis):
+    return gis.content.get(os.getenv('REACH_POINTS_ID'))
 
 
 @pytest.fixture
